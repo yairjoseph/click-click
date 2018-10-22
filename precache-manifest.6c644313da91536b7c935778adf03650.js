03f8c5de35f95c52424f7f3cfdbd64b989d35231
YJ <yair.yoseph@gmail.com>
2018-10-22 16:57:23 -0700

    Updates

---
+++ precache-manifest.6c644313da91536b7c935778adf03650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "be3d3993e649c90c3e174e2d90ae7433",
+    "url": "/click-click/static/media/clouds.be3d3993.jpg"
+  },
+  {
+    "revision": "e1eb0ad1e1bdda0ab110",
+    "url": "/click-click/static/js/runtime~main.e1eb0ad1.js"
+  },
+  {
+    "revision": "a99e0e6db88f59b7b8d5",
+    "url": "/click-click/static/js/main.a99e0e6d.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/click-click/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "a99e0e6db88f59b7b8d5",
+    "url": "/click-click/static/css/main.31e85a51.chunk.css"
+  },
+  {
+    "revision": "cf8219233b9ebb6434d7cb5fb61a48c1",
+    "url": "/click-click/index.html"
+  }
+];
