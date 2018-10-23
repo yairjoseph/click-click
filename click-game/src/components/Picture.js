ac8c937b884d26afa813fade68b75707e8570e72
YJ <yair.yoseph@gmail.com>
2018-10-23 16:06:09 -0700

    iphone responsive

--- click-game/src/components/Picture.js
+++ click-game/src/components/Picture.js
@@ -4,11 +4,13 @@ const Picture = (props) => {
 // console.log(props)
     return (
 
-        <div className="card" style={{ maxWidth: "200px", maxHeight: "200px", margin: "30px", borderRadius:20, padding:10}}>
-            <img className="card-img-top" src={props.image} id= {props.id} alt="game-img" style={{ width:"150px", cursor: "pointer"}} />
+        <div className="card card-style">
+            <img className="card-img-top" src={props.image} id= {props.id} alt="game-img"  />
         </div>
 
     )
 }
 
 export default Picture;
+// style={{ maxWidth: "200px", maxHeight: "200px", margin: "30px", borderRadius:20, padding:10}}
+// style={{ width:"150px", cursor: "pointer"}}
